539f0c55886300025642ac68d4320e6dc83885a7
macbook <macbook@macbooks-MacBook-Air.local>
2024-12-23 00:59:01 +0100

    fork

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,201 @@
+import { Canvas } from "@react-three/fiber";
+import "./hero.css";
+import Speech from "./Speech";
+import { motion } from "motion/react";
+import Shape from "./Shape";
+import { Suspense } from "react";
+
+const awardVariants = {
+  initial: {
+    x: -100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const followVariants = {
+  initial: {
+    y: -100,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hSection left">
+        {/* TITLE */}
+        <motion.h1
+          initial={{ y: -100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
+          className="hTitle"
+        >
+          Welcome to
+          <br />
+          <span>Roots & Sunshine</span>
+        </motion.h1>
+        {/* AWARDS */}
+        <motion.div
+          variants={awardVariants}
+          initial="initial"
+          animate="animate"
+          className="awards"
+        >
+          <motion.h2 variants={awardVariants}>Secret community marketplace</motion.h2>
+          <motion.p variants={awardVariants}>
+            Top quality shit in the game.
+          </motion.p>
+          <motion.div variants={awardVariants} className="awardList">
+            <motion.img variants={awardVariants} src="/award1.png" alt="" />
+            <motion.img variants={awardVariants} src="/award2.png" alt="" />
+            <motion.img variants={awardVariants} src="/award3.png" alt="" />
+          </motion.div>
+        </motion.div>
+        {/* SCROLL SVG */}
+        <motion.a
+          animate={{ y: [0, 5], opacity: [0, 1, 0] }}
+          transition={{
+            repeat: Infinity,
+            duration: 4,
+            ease: "easeInOut",
+          }}
+          href="#services"
+          className="scroll"
+        >
+          <svg
+            width="50px"
+            height="50px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M5 9C5 5.13401 8.13401 2 12 2C15.866 2 19 5.13401 19 9V15C19 18.866 15.866 22 12 22C8.13401 22 5 18.866 5 15V9Z"
+              stroke="white"
+              strokeWidth="1"
+            />
+            <motion.path
+              animate={{ y: [0, 5] }}
+              transition={{
+                repeat: Infinity,
+                duration: 4,
+                ease: "easeInOut",
+              }}
+              d="M12 5V8"
+              stroke="white"
+              strokeWidth="1"
+              strokeLinecap="round"
+            />
+          </svg>
+        </motion.a>
+      </div>
+      <div className="hSection right">
+        {/* FOLLOW */}
+        <motion.div
+          variants={followVariants}
+          initial="initial"
+          animate="animate"
+          className="follow"
+        >
+          <motion.a variants={followVariants} href="/">
+            <img src="/instagram.png" alt="" />
+          </motion.a>
+          <motion.a variants={followVariants} href="/">
+            <img src="/facebook.png" alt="" />
+          </motion.a>
+          <motion.a variants={followVariants} href="/">
+            <img src="/youtube.png" alt="" />
+          </motion.a>
+          <motion.div variants={followVariants} className="followTextContainer">
+            <div className="followText">FOLLOW US</div>
+          </motion.div>
+        </motion.div>
+        {/* BUBBLE */}
+        <Speech />
+        {/* CERTIFICATE */}
+        <motion.div
+          animate={{ opacity: [0, 1] }}
+          transition={{ duration: 1 }}
+          className="certificate"
+        >
+          <img src="/certificate.png" alt="" />
+          BIO
+          <br />
+          FAIR TRADE
+          <br />
+          ORGANIC
+        </motion.div>
+        {/* CONTACT BUTTON */}
+        <motion.a
+          href="/#contact"
+          className="contactLink"
+          animate={{
+            x: [200, 0],
+            opacity: [0, 1],
+          }}
+          transition={{
+            duration: 2,
+          }}
+        >
+          <motion.div
+            className="contactButton"
+            animate={{ rotate: [0, 360] }}
+            transition={{
+              duration: 10,
+              repeat: Infinity,
+              ease: "linear",
+            }}
+          >
+           <svg viewBox="0 0 200 200" width="150" height="150">
+  <circle cx="100" cy="100" r="90" fill="transparent" />
+  <path
+    id="innerCirclePath"
+    fill="none"
+    d="M 100,100 m -60,0 a 60,60 0 1,1 120,0 a 60,60 0 1,1 -120,0"
+  />
+  <text style={{ fill: "gold" }} className="circleText">
+    <textPath href="#innerCirclePath">Buy Now •</textPath>
+  </text>
+  <text style={{ fill: "gold" }} className="circleText">
+    <textPath href="#innerCirclePath" startOffset="44%">
+      Contact Us •
+    </textPath>
+  </text>
+</svg>
+
+           
+          </motion.div>
+        </motion.a>
+      </div>
+      <div className="bg">
+        {/* 3d */}
+        <Canvas>
+          <Suspense fallback="loading...">
+            <Shape />
+          </Suspense>
+        </Canvas>
+        <div className="hImg">
+          <img src="/hero.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/hero/Speech.jsx
@@ -0,0 +1,32 @@
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "motion/react";
+
+const Speech = () => {
+  return (
+    <motion.div
+      className="bubbleContainer"
+      animate={{ opacity: [0, 1] }}
+      transition={{ duration: 1 }}
+    >
+      <div className="bubble">
+        <TypeAnimation
+          sequence={[
+            1000,
+            "Become a part of our family - get high on life and our shit!",
+            1000,
+            "Don't hesitate to reach out to us!",
+            1000,
+          ]}
+          wrapper="span"
+          speed={40}
+          deletionSpeed={70}
+          // omitDeletionAnimation
+          repeat={Infinity}
+        />
+      </div>
+      <img src="/man.png" alt="" />
+    </motion.div>
+  );
+};
+
+export default Speech;
